b8d9180324aba593984ebd5b5d9e9e97be023e39
Quentin Lintz <quentinlintz@outlook.com>
2024-03-26 22:35:49 -0400

    Create basic root page

---
+++ src/components/nibble-form.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+
+const NibbleFormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export function NibbleForm() {
+  const form = useForm<z.infer<typeof NibbleFormSchema>>({
+    resolver: zodResolver(NibbleFormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof NibbleFormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Create Your AI Study Guide</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your topic here" {...field} />
+              </FormControl>
+              <FormDescription>
+                Input a topic you&apos;re interested in and let our AI generate
+                a personalized study guide for you.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Generate Study Guide</Button>
+      </form>
+    </Form>
+  );
+}
