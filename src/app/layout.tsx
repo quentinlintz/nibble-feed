c36a924b44aa5d49776b7107bb3aee5486c5e8ba
Quentin Lintz <quentinlintz@outlook.com>
2024-04-03 20:45:54 -0400

    Make header a client component for static caching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-100`}>
-        <Header />
-        <main>{children}</main>
+        <Providers>
+          <Header />
+          <main>{children}</main>
+        </Providers>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,47 @@
+"use client";
+
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { auth } from "@/auth";
-import Avatar from "./avatar";
+import { useSession } from "next-auth/react";
+import {
+  Avatar as ReactAvatar,
+  AvatarImage,
+  AvatarFallback,
+} from "@radix-ui/react-avatar";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover";
+import { Button } from "./ui/button";
 import * as actions from "@/actions";
 
-export default async function Header() {
-  const session = await auth();
+export default function Header() {
+  const session = useSession();
 
   let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = <Avatar src={session.user.image ?? ""} />;
+  if (session.status === "loading") {
+    authContent = null;
+  } else if (session.data?.user) {
+    authContent = (
+      <Popover>
+        <PopoverTrigger>
+          <ReactAvatar>
+            <AvatarImage
+              src={session.data?.user.image || ""}
+              className="w-10 h-10 rounded-full"
+            />
+            <AvatarFallback>NF</AvatarFallback>
+          </ReactAvatar>
+        </PopoverTrigger>
+        <PopoverContent className="m-8">
+          <form action={actions.signOut}>
+            <Button size="sm" variant="destructive">
+              Sign Out
+            </Button>
+          </form>
+        </PopoverContent>
+      </Popover>
+    );
   } else {
     authContent = (
       <form action={actions.signIn}>
@@ -19,6 +51,19 @@ export default async function Header() {
       </form>
     );
   }
+
+  <header className="fixed inset-x-0 top-0 h-16 z-50 flex items-center px-4 sm:px-6 lg:px-8 bg-opacity-90 backdrop-blur-sm border-b border-gray-200 backdrop-filter dark:border-gray-800 dark:bg-gray-950/90">
+    <nav className="flex-1 flex items-center justify-between">
+      <Link className="flex items-center" href="/">
+        <span className="ml-2 font-semibold sm:text-base lg:text-lg">
+          Nibble Feed
+        </span>
+      </Link>
+      <div className="flex flex-1 justify-end items-center space-x-4">
+        {authContent}
+      </div>
+    </nav>
+  </header>;
 
   return (
     <header className="fixed inset-x-0 top-0 h-16 z-50 flex items-center px-4 sm:px-6 lg:px-8 bg-opacity-90 backdrop-blur-sm border-b border-gray-200 backdrop-filter dark:border-gray-800 dark:bg-gray-950/90">
